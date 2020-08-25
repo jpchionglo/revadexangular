f00ccb7cf9e96e14fa28e1676ae806f6dbe957e5
Jeremy Chionglo <jpchionglo@gmail.com>
2020-08-25 17:35:53 -0500

    added stuff to home component

--- stardex/src/app/home/home.component.ts
+++ stardex/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../services/image.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constellation: string;
+  mapurl: string;
+
+  constructor(private img: ImageService) { }
 
   ngOnInit(): void {
+
+    this.constellation = 'something';
+    this.mapurl = 'http://www.radicalcartography.net/stars4.png';
+
   }
 
 }
