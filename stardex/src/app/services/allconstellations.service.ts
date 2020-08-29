d318da950b323fb79452e1cf3543671de8a3f4bf
Jeremy Chionglo <jpchionglo@gmail.com>
2020-08-29 16:27:39 -0500

    working on async reading o json

--- stardex/src/app/services/allconstellations.service.ts
+++ stardex/src/app/services/allconstellations.service.ts
@@ -2,26 +2,33 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Constellation } from '../models/constellation';
 import { RequestService } from './request.service';
+import { Observable } from 'rxjs';
+import { timer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AllconstellationsService {
 
+  obv$: Observable<string>;
   obvString: string;
 
   constructor(private http: HttpClient, private req: RequestService) { }
 
   getConstellations(): Constellation[] {
 
-    let observe = this.req.getJSON("https://domainserviceapi.azurewebsites.net/api/Constellation");
+    this.obv$ = this.req.getJSON("https://domainserviceapi.azurewebsites.net/api/Constellation");
 
-    observe.subscribe(event => this.obvString = event);
+    this.obv$.subscribe((res) => {
+
+      this.obvString = res;
+
+    });
+
+    console.log(this.obvString)
     
-    console.log(this.obvString);
-
     let jObj: any = JSON.parse(this.obvString);
-
+    
     return <Constellation[]>jObj;
 
   }
