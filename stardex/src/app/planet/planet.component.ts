a2e60afcac5c939dc075f043c897e354d0b84cf5
Jeremy Chionglo <jpchionglo@gmail.com>
2020-08-31 10:58:53 -0500

    Finished MVP

--- stardex/src/app/planet/planet.component.ts
+++ stardex/src/app/planet/planet.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ImageService } from '../services/image.service';
 import { ImageUrlData } from '../data/Image_data.model';
 import { Observable } from 'rxjs';
+import { Planet } from '../models/planet';
 
 
 @Component({
@@ -10,21 +11,29 @@ import { Observable } from 'rxjs';
   styleUrls: ['./planet.component.css']
 })
 export class PlanetComponent implements OnInit {
-
+  @Input() planet: Planet;
+  
   name: string;
   image$: Observable<string>;
   image: string;
+  should_open: boolean;
 
   constructor(private img: ImageService) { }
 
   ngOnInit(): void {
-    ///Image
-    let name = 'Mercury'
-    this.image$ = this.img.getImageUrl(name);
+    
+    this.name = this.planet.Name;
+    this.image$ = this.img.getImageUrl(this.name);
     this.image$.subscribe((res) => {
       this.image = res;
     })
-    ///
+
+    this.should_open = false;
+    
+  }
+
+  toggleChild(): void {
+    this.should_open = !this.should_open;
   }
 
 }
--- stardex/src/app/star/star.component.ts
+++ stardex/src/app/star/star.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ImageService } from '../services/image.service';
 import { Observable } from 'rxjs';
 import { ImageUrlData } from '../data/Image_data.model';
+import { Star } from '../models/star';
 
 @Component({
   selector: 'app-star',
@@ -9,23 +10,43 @@ import { ImageUrlData } from '../data/Image_data.model';
   styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnInit {
+  @Input() star: Star;
 
   name: string;
   image$: Observable<string>;
   image: string;
+  should_open: boolean;
+  showdistance: boolean;
+  showtemperature: boolean;
+  showmass: boolean;
 
   constructor(private img: ImageService) { }
 
   ngOnInit(): void {
     ///Image
-    let name = ''
-    this.image$ = this.img.getImageUrl(name);
+    this.name = this.star.Name;
+    this.image$ = this.img.getImageUrl(this.name);
     this.image$.subscribe((res) => {
       this.image = res;
-    ///
-
-
     })
+    this.should_open = false;
+
+    if (this.star.Distance > 0){
+      this.showdistance = true;
+    }
+
+    if (this.star.Temperature > 0){
+      this.showtemperature = true;
+    }
+
+    if (this.star.Mass > 0){
+      this.showmass = true;
+    }
+
+  }
+
+  toggleChild(): void {
+    this.should_open = !this.should_open;
   }
 
 }
